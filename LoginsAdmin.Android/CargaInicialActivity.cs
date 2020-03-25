94566917c27ea80aad1cbfaab1133d3bdaadb5a7
Jonatandb <jonatandb@gmail.com>
2020-03-25 15:31:32 -0300

    Agrego pantalla de carga inicial según esta guía:
    https://docs.microsoft.com/es-es/xamarin/android/user-interface/splash-screen

---
+++ LoginsAdmin.Android/CargaInicialActivity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace LoginsAdmin.Droid
+{
+    [Activity(Theme = "@style/LoginsAdmin.PantallaDeCargaInicial", MainLauncher = true, NoHistory = true, Label = "LoginsAdmin", Icon = "@mipmap/icon" )]
+    public class CargaInicialActivity : AppCompatActivity
+    {
+        //protected override void OnCreate(Bundle savedInstanceState)
+        //{
+        //    base.OnCreate(savedInstanceState);
+
+        //    // Create your application here
+        //}
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { StartMainActivityOnBackground(); });
+            startupWork.Start();
+        }
+
+        void StartMainActivityOnBackground()
+        {
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        public override void OnBackPressed() { }
+
+    }
+}
